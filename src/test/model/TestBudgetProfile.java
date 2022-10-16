14cd9e8f3bdebb123fe397f11c2752b88508b4ce
Roman Sinkus <rsinkus@students.cs.ubc.ca>
2022-10-15 21:34:28 -0700

    Renamed both test classes to include a capital "T" at the beginning instead of lowercase (convention and so that autoTest can provide accurate code coverage report).

--- src/test/model/TestBudgetProfile.java
+++ src/test/model/TestBudgetProfile.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class testBudgetProfile {
+public class TestBudgetProfile {
 
     private BudgetProfile profile1;
     private BudgetSection section1;
